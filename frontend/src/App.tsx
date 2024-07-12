09e83e98c3b5b8e4ddab8c4efa53284aeddb7ea9
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-07-12 12:22:56 +0300

    added 3 pages to the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
+import { Landingpage } from './pages/landingpage'
+import { Addproduct } from './pages/addproduct'
+import { Orderpage } from './pages/orderpage'
 import { Register, Login } from './pages/Authentication'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
@@ -13,6 +16,9 @@ function App() {
     <Navbar />
     <div>
     <Routes>
+      <Route index element={<Landingpage/>}/>
+      <Route path="/order" element={<Orderpage/>}/>
+      <Route path="/addproduct" element={<Addproduct/>}/>
       <Route path='/home' element={ <HomePage />}/>
       <Route path='/register' element={ <Register />}/>
       <Route path='/login' element={ <Login />}/>
